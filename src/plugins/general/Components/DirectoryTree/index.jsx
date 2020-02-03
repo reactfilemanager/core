32cd6c012bf021cbada2c050d1f488c696714c12
Parvez Akther <parvez@themexpert.com>
2020-02-03 14:53:15 +0600

    Working with tree

--- src/plugins/general/Components/DirectoryTree/index.jsx
+++ src/plugins/general/Components/DirectoryTree/index.jsx
@@ -1,8 +1,31 @@
 import React, {Component} from 'react';
+import { Label, Spinner } from 'theme-ui'
 import Tree, {TreeNode} from 'rc-tree';
 import {resetDirectoryTree, setWorkingPath} from '../../state/actions';
 import {getApi} from '../../tools/config';
+import icons from '../../../../assets/icons';
+import './style.scss'
 
+const arrowPath = 'M869 487.8L491.2 159.9c-2.9-2.5-6.6-3.9-10.5-3.9h-88' +
+  '.5c-7.4 0-10.8 9.2-5.2 14l350.2 304H152c-4.4 0-8 3.6-8 8v60c0 4.4 3.' +
+  '6 8 8 8h585.1L386.9 854c-5.6 4.9-2.2 14 5.2 14h91.5c1.9 0 3.8-0.7 5.' +
+  '2-2L869 536.2c14.7-12.8 14.7-35.6 0-48.4z';
+
+const getSvgIcon = (path, iStyle = {}, style = {}) => {
+  return (
+    <i style={iStyle}>
+      <svg
+        viewBox="0 0 1024 1024"
+        width="1em"
+        height="1em"
+        fill="currentColor"
+        style={{ verticalAlign: '-.125em', ...style }}
+      >
+        <path d={path} />
+      </svg>
+    </i>
+  );
+}
 class DirectoryTree extends Component {
   state = {
     dirs: [],
@@ -124,11 +147,7 @@ class DirectoryTree extends Component {
   };
 
   sendIcon = props => {
-    return props.loading
-        ? <i className="fa fa-spin fa-spinner"/>
-        : props.expanded
-            ? <i className="far fa-folder-open"/>
-            : <i className="far fa-folder"/>;
+    return props.loading ? <Spinner/> : icons.folder ;
   };
 
   render() {
@@ -154,19 +173,33 @@ class DirectoryTree extends Component {
     };
 
     const _dirs = loop(this.state.dirs);
+    
+    const switcherIcon = (obj) => {
+      return getSvgIcon(arrowPath,
+        { cursor: 'pointer', backgroundColor: '#eee' },
+        { transform: `rotate(${obj.expanded ? 90 : 0}deg)` });
+    };
     return (
-        <div>
+        <>
+          <Label sx={{
+            px: 3,
+            py: 3,
+            textTransform: 'uppercase',
+            fontWeight: 'heading',
+            fontSize: 11,
+          }}>Folders</Label>
           <Tree
               loadData={this.onLoadData}
               onSelect={this.onSelect}
               checkable={false}
               selectedKeys={[path]}
               icon={this.sendIcon}
+              switcherIcon={switcherIcon}
               showLine
           >
             {_dirs}
           </Tree>
-        </div>
+        </>
     );
   }
 }
