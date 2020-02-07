da345945a7ac6820493147cf6764fb85dda1c43e
Rocky <wpkpda@gmail.com>
2020-02-07 23:40:24 +0600

    don't show nav if there's only one

--- src/core/FileManager/index.jsx
+++ src/core/FileManager/index.jsx
@@ -16,65 +16,65 @@ export default () => {
   let activeFirst = false;
   for (const tab of tabs) {
     navs.push(
-      <Link
-        id={tab.key + '-tab'}
-        href={'#' + tab.key}
-        role="tab"
-        aria-controls={tab.key}
-        aria-selected="true"
-        sx={{
-          px: 4,
-          py: 2,
-          bg: '#fafbfb',
-          textDecoration: 'none',
-          color: 'gray',
-          fontSize: 12,
-          textTransform: 'uppercase'
-        }}
-      >
-        {tab.title}
-      </Link>,
+        <Link
+            id={tab.key + '-tab'}
+            key={tab.key}
+            href={'#' + tab.key}
+            role="tab"
+            aria-controls={tab.key}
+            aria-selected="true"
+            sx={{
+              px: 4,
+              py: 2,
+              bg: '#fafbfb',
+              textDecoration: 'none',
+              color: 'gray',
+              fontSize: 12,
+              textTransform: 'uppercase',
+            }}
+        >
+          {tab.title}
+        </Link>,
     );
     contents.push(
-        <Box 
-          key={tab.key}
-          id={tab.key}
-          role="tabpanel"
-          aria-selected={activeFirst ? 'false' : 'true'}
-          aria-labelledby={tab.key + '-tab'}
+        <Box
+            key={tab.key}
+            id={tab.key}
+            role="tabpanel"
+            aria-selected={activeFirst ? 'false' : 'true'}
+            aria-labelledby={tab.key + '-tab'}
         >
           <tab.component/>
         </Box>,
     );
-    // if (!activeFirst) {
-    //   setTimeout(() => $('#' + tab.key + '-tab').tab('show'), 100);
-    //   activeFirst = true;
-    // }
   }
 
   return (
-    <Box>
-      <Flex sx={{
-      bg : '#f5f5f5'
-    }}>
-        {navs}
-      </Flex>
       <Box>
-      {contents}
-      </Box>
+        {navs.length > 1
+            ? <Flex sx={{
+              bg: '#f5f5f5',
+            }}>
+              {navs}
+            </Flex>
+            : null
+        }
+        <Box>
+          {contents}
+        </Box>
 
-      <ToastContainer
-        position="top-right"
-        autoClose={2500}
-        hideProgressBar
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        pauseOnVisibilityChange
-        draggable
-        pauseOnHover
-        transition={Flip}
-      />
-    </Box>
+        <ToastContainer
+            position="top-right"
+            autoClose={2500}
+            hideProgressBar
+            newestOnTop
+            closeOnClick
+            rtl={false}
+            pauseOnVisibilityChange
+            draggable
+            pauseOnHover
+            transition={Flip}
+        />
+      </Box>
   );
 }
