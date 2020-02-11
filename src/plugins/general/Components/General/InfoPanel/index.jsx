05c1d83f8eecded411ac00c5ca101b81c5862f4d
Parvez Akther <parvez@themexpert.com>
2020-02-11 12:42:28 +0600

    Info panel done

--- src/plugins/general/Components/General/InfoPanel/index.jsx
+++ src/plugins/general/Components/General/InfoPanel/index.jsx
@@ -1,71 +1,115 @@
 /** @jsx jsx*/
-import {jsx, Spinner} from 'theme-ui';
+import {jsx, Box, Heading, Text, Close } from 'theme-ui';
 import React, {Component} from 'react';
+import styled from '@emotion/styled';
 import icons from '../../../../../assets/icons';
 import {FILE_TYPES} from '../../Toolbar/FilterByType';
 
 class InfoPanel extends Component {
 
+  state = {isOpen: true};
+
   get isImage() {
     return FILE_TYPES.image.indexOf(this.props.item.extension) >= 0;
   }
 
+  closePanel = () => {
+    this.setState({ isOpen: false })
+  };
+
   render() {
     const item = this.props.item;
     return (
-        <table>
-          <tbody>
-          <tr>
-            <td colSpan={2}><h3> {icons.info} Details</h3></td>
-          </tr>
-          <tr>
-            <td>Name</td>
-            <td>{item.name}</td>
-          </tr>
-          <tr>
-            <td>Size</td>
-            <td>{item.size.toHumanFileSize()}</td>
-          </tr>
-          <tr>
-            <td>Permission</td>
-            <td>{item.perms}</td>
-          </tr>
-          <tr>
-            <td>Last Modified</td>
-            <td>{item.last_modified.toHumanFormat()}</td>
-          </tr>
-          {this.isImage ?
-              <>
-                <tr>
-                  <td>Dimension</td>
-                  <td>
-                    {`${item.image_info.width}x${item.image_info.width}`}
-                  </td>
-                </tr>
-                <tr>
-                  <td>Bits</td>
-                  <td>
-                    {item.image_info.bits}
-                  </td>
-                </tr>
-                <tr>
-                  <td>Channels</td>
-                  <td>
-                    {item.image_info.channels}
-                  </td>
-                </tr>
-                <tr>
-                  <td>Mime Type</td>
-                  <td>
-                    {item.image_info.mime}
-                  </td>
-                </tr>
-              </>
-              : null}
-          </tbody>
-        </table>
+      <Box sx={{
+        position: 'fixed',
+        top: 0,
+        right: 0,
+        width: '320px',
+        height: '100%',
+        bg: 'white',
+        p: 3,
+        borderLeft: '1px solid #ddd',
+        transition: 'transform 0.2s ease-out',
+        transform: (this.state.isOpen) ? 'translateX(0)' : 'translateX(340px)'
+      }}>
+        <Heading as='h2'>{icons.info} Details</Heading>
+        <Box sx={{ marginTop: 4 }}>
+          <Block>
+            <Label>Name</Label>
+            <Text sx={{ wordBreak: 'break-word' }}>{item.name}</Text>
+          </Block>
+          {this.isImage ? <>
+            <Block>
+              <Label>Dimension</Label>
+              <Text>{`${item.image_info.width}x${item.image_info.width}`}</Text>
+            </Block>
+          </> : null}
+          <Block>
+            <Label>Size</Label>
+            <Text>{item.size.toHumanFileSize()}</Text>
+          </Block>
+          <Block>
+            <Label>Permission</Label>
+            <Text>{item.perms}</Text>
+          </Block>
+          <Block>
+            <Label>Last Modified</Label>
+            <Text>{item.last_modified.toHumanFormat()}</Text>
+          </Block>
+        </Box>
+        
+        <Close sx={{
+          position: 'absolute',
+          top: '10px',
+          right: '10px'
+        }} onClick={this.closePanel} />
+      </Box>
+        
+        // <table>
+        //   <tbody>
+        //   {this.isImage ?
+        //       <>
+        //         <tr>
+        //           <td>Dimension</td>
+        //           <td>
+        //             {`${item.image_info.width}x${item.image_info.width}`}
+        //           </td>
+        //         </tr>
+        //         <tr>
+        //           <td>Bits</td>
+        //           <td>
+        //             {item.image_info.bits}
+        //           </td>
+        //         </tr>
+        //         <tr>
+        //           <td>Channels</td>
+        //           <td>
+        //             {item.image_info.channels}
+        //           </td>
+        //         </tr>
+        //         <tr>
+        //           <td>Mime Type</td>
+        //           <td>
+        //             {item.image_info.mime}
+        //           </td>
+        //         </tr>
+        //       </>
+        //       : null}
+        //   </tbody>
+        // </table>
     );
   }
 }
 
 export default InfoPanel;
+
+const Block = styled.div`
+  display: flex;
+  padding: 8px 0;
+  font-size: 13px;
+  line-height: 20px;
+`
+const Label = styled.div`
+  flex: 0 0 120px;
+  color: #999;
+`
