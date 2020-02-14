0069f1d1d505ffd866a8c2cca13a0fbe0ad7519c
Rocky <wpkpda@gmail.com>
2020-02-14 13:06:57 +0600

    build refactor

--- build.js
+++ build.js
@@ -1,40 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-import FileManager, {setConfig, Pluggable} from './src/file-manager';
-import icons from './src/assets/icons';
-import {toast} from 'react-toastify';
-import Core, {CORE_PLUGIN_KEY} from './src/core/plugin';
-import ImagePreview from './src/plugins/image_preview/Components/ImagePreview';
-import AudioPlayer, {injection as AudioPlayerInjection} from './src/plugins/audio_player';
-
-const ROOT_URL = 'http://127.0.0.1:8000/tmp/storage';
-const core = Pluggable.plugin(CORE_PLUGIN_KEY);
-
-// register audio player
-const audio_player = Pluggable.registerPlugin('audio_player', AudioPlayer);
-// set audio path resolver
-audio_player.accessor().setPathResolver(item => {
-  return ROOT_URL + item.path;
-});
-
-// inject audio player into core
-core.inject(AudioPlayerInjection);
-
-// Copy URL context menu
-core.addContextMenu(
-    'copy_url',
-    (item, state) => {
-      return item.is_file;
-    },
-    (item, state, dispatch) => {
-      `${ROOT_URL}${item.path}`.copyToClipboard();
-      toast.info('URL Copied!');
-    },
-    {
-      icon: icons.link,
-      title: 'Copy URL',
-    },
-);
+import FileManager, {setConfig} from './src/file-manager';
 
 // Selection handler on file select mode
 // generalPlugin.addHandler(
@@ -51,6 +17,7 @@ setConfig({
   // URL of the server installation
   url: 'http://127.0.0.1:8000/',
   // HTTP request modifiers
+  root_url: 'http://127.0.0.1:8000/tmp/storage',
   http: {
     query_params: {
       foo: 'bar',
--- src/file-manager.js
+++ src/file-manager.js
@@ -11,6 +11,9 @@ import {
 import {Pluggable} from './pluggable';
 import Core, {CORE_PLUGIN_KEY} from './core/plugin';
 import ImagePreview from './plugins/image_preview/Components/ImagePreview';
+import AudioPlayer, {injection as AudioPlayerInjection} from './plugins/audio_player';
+import {toast} from 'react-toastify';
+import icons from './assets/icons';
 
 // core
 const core = Pluggable.registerPlugin(CORE_PLUGIN_KEY, Core);
@@ -20,6 +23,12 @@ core.inject(ImagePreview);
 
 const setConfig = (config = {}) => {
   setBaseUrl(config.url);
+  if (config.root_url) {
+    registerAudioPlayer(core, config.root_url);
+    registerVideoPlayer(core, config.root_url);
+    registerCopyURLMenuItem(core, config.root_url);
+  }
+
   if (config.http) {
     if (config.http.headers) {
       setHeaders(config.http.headers);
@@ -32,6 +41,40 @@ const setConfig = (config = {}) => {
     }
   }
 };
+
+function registerAudioPlayer(core, ROOT_URL) {
+  // register audio player
+  const audio_player = Pluggable.registerPlugin('audio_player', AudioPlayer);
+  // set audio path resolver
+  audio_player.accessor().setPathResolver(item => {
+    return ROOT_URL + item.path;
+  });
+
+  // inject audio player into core
+  core.inject(AudioPlayerInjection);
+}
+
+function registerVideoPlayer(core, ROOT_URL) {
+
+}
+
+function registerCopyURLMenuItem(core, ROOT_URL) {
+  // Copy URL context menu
+  core.addContextMenu(
+      'copy_url',
+      (item, state) => {
+        return item.is_file;
+      },
+      (item, state, dispatch) => {
+        `${ROOT_URL}${item.path}`.copyToClipboard();
+        toast.info('URL Copied!');
+      },
+      {
+        icon: icons.link,
+        title: 'Copy URL',
+      },
+  );
+}
 
 export {
   Pluggable,
