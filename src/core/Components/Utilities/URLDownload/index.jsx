322850ea2bd0b44a6cd41f6e07fbc165a57dead5
Abu Huraira <shams.bd71@gmail.com>
2020-03-19 17:53:26 +0600

    wrapper for free/pro detection

--- src/core/Components/Utilities/URLDownload/index.jsx
+++ src/core/Components/Utilities/URLDownload/index.jsx
@@ -73,21 +73,26 @@ class RemoteUpload extends Component {
 
           <Text sx={{fontSize: 22, py: 2}}>Upload From URL</Text>
 
-          <Input
-              sx={{lineHeight: 2}}
-              placeholder="Enter remote file url here"
-              autoFocus
-              ref="remote_url"
-              onKeyDown={this.handleKeyDown}
-          />
+          <div className="uploadFromUrlWrapper" style={{
+            width:'100%',textAlign: 'center'
+          }}>
+            <Input
+                sx={{lineHeight: 2}}
+                placeholder="Enter remote file url here"
+                autoFocus
+                ref="remote_url"
+                onKeyDown={this.handleKeyDown}
+            />
 
-          <Button
-              sx={{py: 2, px: 5, marginTop: 3}}
-              onClick={this.handleUpload}
-              disabled={this.state.working}
-          >
-            {this.state.working ? <Spinner/> : 'Download Now'}
-          </Button>
+            <Button
+                sx={{py: 2, px: 5, marginTop: 3, display: 'inline-block'}}
+                onClick={this.handleUpload}
+                disabled={this.state.working}
+            >
+              {this.state.working ? <Spinner/> : 'Download Now'}
+            </Button>
+
+          </div>
 
         </Flex>
     );
