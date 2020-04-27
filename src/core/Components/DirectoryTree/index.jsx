39dbed7fd35c0abcd3e17eae14a53de253601d86
Rocky <wpkpda@gmail.com>
2020-04-27 09:30:42 +0600

    disabled auto open directory in tree

--- src/core/Components/DirectoryTree/index.jsx
+++ src/core/Components/DirectoryTree/index.jsx
@@ -1,24 +1,8 @@
 import React, {Component} from 'react';
 import {setWorkingPath} from '../../state/actions';
 import SelectableDirectoryTree from './SelectableDirectoryTree';
-import {EventBus} from '../../../helpers/Utils';
-import {SET_WORKING_PATH} from '../../state/types';
 
 class DirectoryTree extends Component {
-
-  state = {path: ''};
-
-  componentDidMount() {
-    EventBus.$on(SET_WORKING_PATH, this.setWorkingPath);
-  }
-
-  componentWillUnmount() {
-    EventBus.$off(SET_WORKING_PATH, this.setWorkingPath);
-  }
-
-  setWorkingPath = path => {
-    this.setState({path});
-  };
 
   onSelect = (info) => {
     if (!info.length) {
@@ -27,11 +11,10 @@ class DirectoryTree extends Component {
     const path = info[0];
     // select event, set path
     setWorkingPath(path);
-    this.setState({path});
   };
 
   render() {
-    return <SelectableDirectoryTree onSelect={this.onSelect} path={this.state.path}/>;
+    return <SelectableDirectoryTree onSelect={this.onSelect} path=''/>;
   }
 }
 
